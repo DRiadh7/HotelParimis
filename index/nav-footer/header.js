1d54a314f307ee03ec134fa4884f0579086101b9
tppro <apo.marin@bbox.fr>
2021-05-21 17:46:28 +0200

    m52- Carrousel JS nice/ correction de la page index/ ajout des liens réseaux sociaux+liens de spages actualités et presse

--- index/nav-footer/header.js
+++ index/nav-footer/header.js
@@ -1,18 +1,16 @@
-var tableau = ['https://images.unsplash.com/photo-1619735497594-d286fd8d2d39?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', 'https://images.unsplash.com/photo-1620117654333-c125fef82817?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTV8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'];
-var img = $('.zone1');
-        
-let interval = setInterval(function(){
-    switch(img.attr('src')){
-        case ('src', 'https://images.unsplash.com/photo-1619782087505-e1544bc70e1e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=766&q=80') : 
-        img.attr('src', tableau[0])
-        break;
 
-        case ('src', tableau[0]) : 
-        img.attr('src', tableau[1])
-        break;
 
-        case ('src', tableau[1]) :
-        img.attr('src', 'https://images.unsplash.com/photo-1619782087505-e1544bc70e1e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=766&q=80')
-        break;
+$('document').ready(function(){
+    img = 0;
+    function slider() {
+        img += 1;
+        $('.zone1').attr("src", "../img/" + img + ".jpg"); // Selection NATURELLE
+        if (img == 5) {
+            img = 0;
+            
+
+        };
     }
-},2200)
+    var timer = setInterval(slider, 4000);
+
+})
